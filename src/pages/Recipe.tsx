7757804fcb15cfdb4d7b00532921b388b89274ea
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-07-18 21:59:17 +0900

    feat. 토글 생성 및 ui 수정

--- src/pages/Recipe.tsx
+++ src/pages/Recipe.tsx
@@ -5,6 +5,7 @@ import { getRecipeData } from '../apis/api';
 import RecipeBox from '../components/recipe/RecipeBox';
 
 const Recipe = () => {
+  // 레시피 data
   const [recipeData, setRecipeData] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
@@ -15,17 +16,34 @@ const Recipe = () => {
         console.error('데이터 불러오기 실패:', error);
       }
     };
-
     fetchData();
   }, []);
+
+  // 토글
+  const [showCategories, setShowCategories] = useState(false);
+  const toggleCategories = () => {
+    setShowCategories(!showCategories);
+  };
+
   return (
     <>
       <PageWrapper>
         <BoxWrapper>
           <TypeWrapper>
-            <p>분류</p>
-            <p>좋아요 많은 순</p>
-            <p>가나다 순</p>
+            <CategoriesParagraph>
+              <p onClick={toggleCategories}>분류</p>
+              {showCategories && (
+                <>
+                  <p>좋아요 많은 순</p>
+                  <p>가나다 순</p>
+                </>
+              )}
+            </CategoriesParagraph>
+
+            <SortParagraph>
+              <p>좋아요 많은 순</p>
+              <p>가나다 순</p>
+            </SortParagraph>
           </TypeWrapper>
           <RecipeWrapper>
             {recipeData.map((recipe: any) => (
@@ -41,17 +59,18 @@ const Recipe = () => {
 export default Recipe;
 
 const PageWrapper = styled.div`
-  /* background-color: yellow; */
   box-sizing: border-box;
   position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
   min-height: calc(100vh - 12.7rem);
+  background-color: ${COLORS.backGround};
 `;
 
 const BoxWrapper = styled.div`
-  width: 95rem;
+  /* background-color: yellow; */
+  width: 90rem;
   /* height: 25rem; */
   /* display: flex; */
   flex-direction: column;
@@ -65,11 +84,27 @@ const TypeWrapper = styled.div`
   width: 100%;
   height: 2rem;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: left;
   flex-wrap: wrap;
+`;
+const CategoriesParagraph = styled.div`
+  display: flex;
+  flex-direction: row;
   & > p {
-    margin-right: 5rem;
+    margin-right: 1.5rem;
+    cursor: pointer;
+    &:hover {
+      color: ${COLORS.blue1};
+    }
+  }
+`;
+const SortParagraph = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  & > p {
+    margin-left: 1.5rem;
     cursor: pointer;
     &:hover {
       color: ${COLORS.blue1};
