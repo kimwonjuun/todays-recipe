32b1409300fa536d4b2c30706def9fac6bb7aa43
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-07-19 09:08:10 +0900

    feat. ScrollToTopButton 기능 추가

---
+++ src/components/common/ScrollToTopButton.tsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import COLORS from '../../styles/colors';
+
+const ScrollToTopButton = () => {
+  const [showButton, setShowButton] = useState<boolean>(false);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  const toggleShowButton = () => {
+    const { scrollY } = window;
+    scrollY > 200 ? setShowButton(true) : setShowButton(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleShowButton);
+
+    return () => {
+      window.removeEventListener('scroll', toggleShowButton);
+    };
+  }, []);
+  return (
+    <>
+      <Button onClick={scrollToTop} showButton={showButton}>
+        위로
+      </Button>
+    </>
+  );
+};
+
+export default ScrollToTopButton;
+
+const Button = styled.div<{ showButton: boolean }>`
+  width: 4.5rem;
+  height: 4.5rem;
+  border-radius: 50%;
+
+  position: fixed;
+  right: 2rem;
+  bottom: 2rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  opacity: ${(props) => (props.showButton ? 1 : 0)};
+  transition: opacity 300ms ease-in-out;
+
+  background-color: ${COLORS.blue2};
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
