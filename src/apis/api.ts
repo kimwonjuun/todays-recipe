ca9d0a32afb59a3c41642c2c8ffcc619df029848
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-07-21 14:25:49 +0900

    fix: firebase 오입력한 키 수정

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -61,10 +61,10 @@ export const getRecipeData = async (): Promise<Recipe[]> => {
   const serviceKey = process.env.REACT_APP_FOODSAFETYKOREA_API_KEY;
   try {
     const firstResponse = await axios.get(
-      `http://openapi.foodsafetykorea.go.kr/api/${serviceKey}/COOKRCP01/json/1/2`
+      `http://openapi.foodsafetykorea.go.kr/api/${serviceKey}/COOKRCP01/json/1/1000`
     );
     const secondResponse = await axios.get(
-      `http://openapi.foodsafetykorea.go.kr/api/${serviceKey}/COOKRCP01/json/1001/1116`
+      `http://openapi.foodsafetykorea.go.kr/api/${serviceKey}/COOKRCP01/json/1001/2000`
     );
     console.log('총 데이터 건 수: ', firstResponse.data.COOKRCP01.total_count);
     console.log(
@@ -114,5 +114,34 @@ export const getRecipeData = async (): Promise<Recipe[]> => {
 //       console.error('API 호출 실패:', error);
 //       throw error;
 //     }
+//   }
+// };
+
+// export const getRecipeData = async (): Promise<Recipe[]> => {
+//   const serviceKey = process.env.REACT_APP_FOODSAFETYKOREA_API_KEY;
+//   try {
+//     const responses = await axios.all([
+//       axios.get(
+//         `http://openapi.foodsafetykorea.go.kr/api/${serviceKey}/COOKRCP01/json/1/1000`
+//       ),
+//       axios.get(
+//         `http://openapi.foodsafetykorea.go.kr/api/${serviceKey}/COOKRCP01/json/1001/2000`
+//       ),
+//     ]);
+
+//     const [firstResponse, secondResponse] = responses;
+//     console.log('총 데이터 건 수: ', firstResponse.data.COOKRCP01.total_count);
+//     console.log(
+//       '데이터: ',
+//       firstResponse.data.COOKRCP01.row.concat(secondResponse.data.COOKRCP01.row)
+//     );
+
+//     const allData = firstResponse.data.COOKRCP01.row.concat(
+//       secondResponse.data.COOKRCP01.row
+//     );
+//     return allData;
+//   } catch (error) {
+//     console.error('API 호출 실패:', error);
+//     throw error;
 //   }
 // };
