0d9b7e53788d6ae8484e09652b726b0aa9ed3702
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-07-21 17:22:55 +0900

    feat. api 컴포넌트 주석 처리 후 Firestore db에 데이터 저장

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -75,78 +75,76 @@ const Admin = () => {
           `http://openapi.foodsafetykorea.go.kr/api/${serviceKey}/COOKRCP01/json/1001/2000`
         ),
       ]);
-      const [res1, res2] = responses;
-      const firstData = res1.data.COOKRCP01.row;
-      const secondData = res2.data.COOKRCP01.row;
-      const allData = [...firstData, ...secondData];
+      const [firstResponse, secondResponse] = responses;
+      const allData = firstResponse.data.COOKRCP01.row.concat(
+        secondResponse.data.COOKRCP01.row
+      );
 
-      console.log('데이터 토탈: ', res1.data.COOKRCP01.total_count);
-      console.log('레시피 데이터: ', allData);
+      console.log('데이터: ', allData);
 
       setRecipeList(allData);
-      console.log('recipeList: ', recipeList);
-      {
-        recipeList.map((item) => {
-          addDoc(collection(dbService, 'recipe-list-2'), {
-            ATT_FILE_NO_MAIN: item?.ATT_FILE_NO_MAIN,
-            ATT_FILE_NO_MK: item?.ATT_FILE_NO_MK,
-            HASH_TAG: item?.HASH_TAG,
-            INFO_CAR: item?.INFO_CAR,
-            INFO_ENG: item?.INFO_ENG,
-            INFO_FAT: item?.INFO_FAT,
-            INFO_NA: item?.INFO_NA,
-            INFO_PRO: item?.INFO_PRO,
-            INFO_WGT: item?.INFO_WGT,
-            MANUAL01: item?.MANUAL01,
-            MANUAL02: item?.MANUAL02,
-            MANUAL03: item?.MANUAL03,
-            MANUAL04: item?.MANUAL04,
-            MANUAL05: item?.MANUAL05,
-            MANUAL06: item?.MANUAL06,
-            MANUAL07: item?.MANUAL07,
-            MANUAL08: item?.MANUAL08,
-            MANUAL09: item?.MANUAL09,
-            MANUAL10: item?.MANUAL10,
-            MANUAL11: item?.MANUAL11,
-            MANUAL12: item?.MANUAL12,
-            MANUAL13: item?.MANUAL13,
-            MANUAL14: item?.MANUAL14,
-            MANUAL15: item?.MANUAL15,
-            MANUAL16: item?.MANUAL16,
-            MANUAL17: item?.MANUAL17,
-            MANUAL18: item?.MANUAL18,
-            MANUAL19: item?.MANUAL19,
-            MANUAL20: item?.MANUAL20,
-            MANUAL_IMG01: item?.MANUAL_IMG01,
-            MANUAL_IMG02: item?.MANUAL_IMG02,
-            MANUAL_IMG03: item?.MANUAL_IMG03,
-            MANUAL_IMG04: item?.MANUAL_IMG04,
-            MANUAL_IMG05: item?.MANUAL_IMG05,
-            MANUAL_IMG06: item?.MANUAL_IMG06,
-            MANUAL_IMG07: item?.MANUAL_IMG07,
-            MANUAL_IMG08: item?.MANUAL_IMG08,
-            MANUAL_IMG09: item?.MANUAL_IMG09,
-            MANUAL_IMG10: item?.MANUAL_IMG10,
-            MANUAL_IMG11: item?.MANUAL_IMG11,
-            MANUAL_IMG12: item?.MANUAL_IMG12,
-            MANUAL_IMG13: item?.MANUAL_IMG13,
-            MANUAL_IMG14: item?.MANUAL_IMG14,
-            MANUAL_IMG15: item?.MANUAL_IMG15,
-            MANUAL_IMG16: item?.MANUAL_IMG16,
-            MANUAL_IMG17: item?.MANUAL_IMG17,
-            MANUAL_IMG18: item?.MANUAL_IMG18,
-            MANUAL_IMG19: item?.MANUAL_IMG19,
-            RCP_NA_TIP: item?.RCP_NA_TIP,
-            RCP_NM: item?.RCP_NM,
-            RCP_PARTS_DTLS: item?.RCP_PARTS_DTLS,
-            RCP_PAT2: item?.RCP_PAT2,
-            RCP_SEQ: item?.RCP_SEQ,
-            RCP_WAY2: item?.RCP_WAY2,
-          });
-        });
-      }
     } catch (error) {
       console.error('레시피 리스트를 가져오지 못했어요. :', error);
+    }
+    {
+      recipeList.map((recipe) => {
+        addDoc(collection(dbService, 'recipe-list'), {
+          ATT_FILE_NO_MAIN: recipe?.ATT_FILE_NO_MAIN,
+          ATT_FILE_NO_MK: recipe?.ATT_FILE_NO_MK,
+          HASH_TAG: recipe?.HASH_TAG,
+          INFO_CAR: recipe?.INFO_CAR,
+          INFO_ENG: recipe?.INFO_ENG,
+          INFO_FAT: recipe?.INFO_FAT,
+          INFO_NA: recipe?.INFO_NA,
+          INFO_PRO: recipe?.INFO_PRO,
+          INFO_WGT: recipe?.INFO_WGT,
+          MANUAL01: recipe?.MANUAL01,
+          MANUAL02: recipe?.MANUAL02,
+          MANUAL03: recipe?.MANUAL03,
+          MANUAL04: recipe?.MANUAL04,
+          MANUAL05: recipe?.MANUAL05,
+          MANUAL06: recipe?.MANUAL06,
+          MANUAL07: recipe?.MANUAL07,
+          MANUAL08: recipe?.MANUAL08,
+          MANUAL09: recipe?.MANUAL09,
+          MANUAL10: recipe?.MANUAL10,
+          MANUAL11: recipe?.MANUAL11,
+          MANUAL12: recipe?.MANUAL12,
+          MANUAL13: recipe?.MANUAL13,
+          MANUAL14: recipe?.MANUAL14,
+          MANUAL15: recipe?.MANUAL15,
+          MANUAL16: recipe?.MANUAL16,
+          MANUAL17: recipe?.MANUAL17,
+          MANUAL18: recipe?.MANUAL18,
+          MANUAL19: recipe?.MANUAL19,
+          MANUAL20: recipe?.MANUAL20,
+          MANUAL_IMG01: recipe?.MANUAL_IMG01,
+          MANUAL_IMG02: recipe?.MANUAL_IMG02,
+          MANUAL_IMG03: recipe?.MANUAL_IMG03,
+          MANUAL_IMG04: recipe?.MANUAL_IMG04,
+          MANUAL_IMG05: recipe?.MANUAL_IMG05,
+          MANUAL_IMG06: recipe?.MANUAL_IMG06,
+          MANUAL_IMG07: recipe?.MANUAL_IMG07,
+          MANUAL_IMG08: recipe?.MANUAL_IMG08,
+          MANUAL_IMG09: recipe?.MANUAL_IMG09,
+          MANUAL_IMG10: recipe?.MANUAL_IMG10,
+          MANUAL_IMG11: recipe?.MANUAL_IMG11,
+          MANUAL_IMG12: recipe?.MANUAL_IMG12,
+          MANUAL_IMG13: recipe?.MANUAL_IMG13,
+          MANUAL_IMG14: recipe?.MANUAL_IMG14,
+          MANUAL_IMG15: recipe?.MANUAL_IMG15,
+          MANUAL_IMG16: recipe?.MANUAL_IMG16,
+          MANUAL_IMG17: recipe?.MANUAL_IMG17,
+          MANUAL_IMG18: recipe?.MANUAL_IMG18,
+          MANUAL_IMG19: recipe?.MANUAL_IMG19,
+          RCP_NA_TIP: recipe?.RCP_NA_TIP,
+          RCP_NM: recipe?.RCP_NM,
+          RCP_PARTS_DTLS: recipe?.RCP_PARTS_DTLS,
+          RCP_PAT2: recipe?.RCP_PAT2,
+          RCP_SEQ: recipe?.RCP_SEQ,
+          RCP_WAY2: recipe?.RCP_WAY2,
+        });
+      });
     }
   };
   return (
