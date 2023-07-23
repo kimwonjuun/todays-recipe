d93edb022eb95dbe6ea356ad9b98acb2ff7cd499
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-07-23 16:52:16 +0900

    feat: 카드 클릭 시 detail/:id로 이동해 렌더링 성공

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,14 +1,45 @@
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import COLORS from '../styles/colors';
+import { Recipe } from '../types/Recipe';
+import { useRecipeData } from '../hooks/useRecipeData';
 
 const Detail = () => {
+  // Recipe/RecipeBox, Search에서 받아온 id
+  const { id } = useParams<{ id: string }>();
+  // 특정 레시피를 담아줄 state
+  const [recipe, setRecipe] = useState<Recipe | null>(null);
+  // useRecipeData를 사용하여 레시피 데이터 받아오기
+  const recipeData = useRecipeData();
+
+  useEffect(() => {
+    const selectedRecipe = recipeData.find(
+      (recipe: Recipe) => recipe.RCP_SEQ === id
+    );
+    if (selectedRecipe) {
+      setRecipe(selectedRecipe);
+    }
+  }, [id, recipeData]);
+
+  if (!recipe) {
+    return <div>안돼...</div>;
+  }
   return (
     <>
       <PageWrapper>
         <RCPWrapper>
-          <RCPImgWrapper></RCPImgWrapper>
-          <RCPingredientWrapper></RCPingredientWrapper>
-          <RCPOrderWrapper></RCPOrderWrapper>
+          <ImgWrapper>
+            <img src={recipe.ATT_FILE_NO_MK} alt={recipe.RCP_NM} />
+          </ImgWrapper>
+          <IngredientWrapper>
+            <h2>Ingredients:</h2>
+            <p>{recipe.RCP_PAT2}</p>
+          </IngredientWrapper>
+          <OrderWrapper>
+            <h2>Instructions:</h2>
+            {/* Display instructions */}
+          </OrderWrapper>
         </RCPWrapper>
         <CommynityWrapper>
           <BookmarkWrapper></BookmarkWrapper>
@@ -39,9 +70,9 @@ const RCPWrapper = styled.div`
   position: relative;
   background-color: red;
 `;
-const RCPImgWrapper = styled.div``;
-const RCPingredientWrapper = styled.div``;
-const RCPOrderWrapper = styled.div``;
+const ImgWrapper = styled.div``;
+const IngredientWrapper = styled.div``;
+const OrderWrapper = styled.div``;
 const CommynityWrapper = styled.div``;
 const BookmarkWrapper = styled.div``;
 const CommentWrapper = styled.div``;
