8c42ba05907c9b7a206d54afc538b4ca04f52499
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-07-25 00:26:35 +0900

    feat: 로그인을 위한 모달창 생성

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,8 +1,19 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import COLORS from '../../styles/colors';
 
 const Header = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <>
       <HeaderWrapper>
@@ -11,12 +22,45 @@ const Header = () => {
             <LogoImg src={require('../../assets/logo.png')}></LogoImg>
           </Link>
         </Logo>
-        <Text>로그인</Text>
+        <Text onClick={openModal}>로그인</Text>
       </HeaderWrapper>
+      {modalIsOpen && (
+        <ModalWrapper>
+          <ModalContent>
+            {/* 로그인, 회원가입 폼을 여기에 작성 */}
+            <button onClick={closeModal}>닫기</button>
+          </ModalContent>
+        </ModalWrapper>
+      )}
     </>
   );
 };
+
 export default Header;
+
+// 나머지 스타일 코드는 동일합니다.
+
+const ModalWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+`;
+
+const ModalContent = styled.div`
+  background-color: white;
+  padding: 2rem;
+  border-radius: 1rem;
+  max-width: 40rem;
+  width: 90%;
+  position: relative;
+`;
 
 const HeaderWrapper = styled.div`
   height: 12.5rem;
