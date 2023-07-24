a131ce0bb8c91890692a54b34bb4cdaf1fb3dbbe
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-07-24 17:12:00 +0900

    design: 디테일 페이지 ui 완성

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -31,57 +31,62 @@ const Detail = () => {
     <>
       <PageWrapper>
         <RecipeDetailWrapper>
-          <ImgTitleWrapper>
-            <ImgWrapper>
-              <Img src={recipe.ATT_FILE_NO_MK} alt={recipe.RCP_NM} />
-            </ImgWrapper>
-            <TItle>
-              {recipe.RCP_PAT2} / {recipe.RCP_NM}
-            </TItle>
-          </ImgTitleWrapper>
+          <Wrapper>
+            <ImgTitleWrapper>
+              <ImgWrapper>
+                <Img src={recipe.ATT_FILE_NO_MK} alt={recipe.RCP_NM} />
+              </ImgWrapper>
+              <TItle>
+                {recipe.RCP_PAT2} / {recipe.RCP_NM}
+              </TItle>
+            </ImgTitleWrapper>
+            <IngredientWrapper>
+              <Ingredient>
+                <h1>재료</h1>
+                <p>{recipe.RCP_PARTS_DTLS.split(',').join(', ')}</p>
+              </Ingredient>
+              <Ingredient>
+                <h1>성분</h1>
+                <p>
+                  중량(1인분):{' '}
+                  {recipe.INFO_WGT ? `${recipe.INFO_WGT} g` : '정보 없음'}
+                </p>
+                <p>열량: {recipe.INFO_ENG} kcal</p>
+                <p>탄수화물: {recipe.INFO_CAR} g</p>
+                <p>단백질: {recipe.INFO_PRO} g</p>
+                <p>지방: {recipe.INFO_FAT} g</p>
+                <p>나트륨: {recipe.INFO_NA} mg</p>
+              </Ingredient>
+            </IngredientWrapper>
+          </Wrapper>
 
-          <IngredientWrapper>
-            <Ingredient>
-              <h1>재료</h1>
-              <p>{recipe.RCP_PARTS_DTLS.split(',').join(', ')}</p>
-            </Ingredient>
-            <Ingredient>
-              <h1>성분</h1>
-              <p>
-                중량(1인분):{' '}
-                {recipe.INFO_WGT ? `${recipe.INFO_WGT} g` : '정보 없음'}
-              </p>
-              <p>열량: {recipe.INFO_ENG} kcal</p>
-              <p>탄수화물: {recipe.INFO_CAR} g</p>
-              <p>단백질: {recipe.INFO_PRO} g</p>
-              <p>지방: {recipe.INFO_FAT} g</p>
-              <p>나트륨: {recipe.INFO_NA} mg</p>
-            </Ingredient>
-          </IngredientWrapper>
           <CookingStepWrapper>
             <h1>조리법</h1>
+            {/* 만드는 방법 최대 길이 20 */}
             {Array.from({ length: 20 }, (_, i) => i + 1).map((step) => {
               const manual = (recipe as any)[
                 `MANUAL${step.toString().padStart(2, '0')}`
-              ]
-                .replace(/^\d+\./g, '')
-                .replace('-', ':');
+              ];
               const manualImg = (recipe as any)[
                 `MANUAL_IMG${step.toString().padStart(2, '0')}`
               ];
+
               return manual ? (
                 <CookingStep key={`step-${step}`}>
                   {manualImg && (
                     <img src={manualImg} alt={`Step ${step}`} width="50%" />
                   )}
                   <p>
-                    Step {step}: {manual}
+                    Step {step}:{' '}
+                    {manual.replace(/^\d+\./g, '').replace('-', ':')}
                   </p>
                 </CookingStep>
               ) : null;
             })}
-            <h2>저감 조리법 TIP:</h2>
-            <p>{recipe.RCP_NA_TIP}</p>
+            <TipWrapper>
+              <h1>저감 조리법 TIP</h1>
+              <p>{recipe.RCP_NA_TIP}</p>
+            </TipWrapper>
           </CookingStepWrapper>
         </RecipeDetailWrapper>
         <CommynityWrapper>
@@ -111,18 +116,27 @@ const RecipeDetailWrapper = styled.div`
   align-items: center;
   font-size: 2rem;
   position: relative;
-  margin: 3rem 0;
+  margin: 4rem 0;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 1rem;
+  margin-bottom: 1rem;
 `;
 
 const ImgTitleWrapper = styled.div`
-  border: 0.25rem solid ${COLORS.blue1};
+  width: 50%;
+  border: 0.25rem solid ${COLORS.blue2};
   border-radius: 1rem;
   background-color: #ffffff;
 `;
 
 const ImgWrapper = styled.div`
-  width: 40rem;
-  height: 40rem;
+  /* width: 35rem; */
+  /* height: 40rem; */
+  width: 100%;
   overflow: hidden;
 `;
 
@@ -130,7 +144,7 @@ const Img = styled.img`
   width: 100%;
   height: 100%;
   border-top-left-radius: 1rem;
-  border-top-right-radius: 1;
+  border-top-right-radius: 1rem;
 `;
 
 const TItle = styled.div`
@@ -140,17 +154,29 @@ const TItle = styled.div`
 `;
 
 const IngredientWrapper = styled.div`
-  width: inherit;
-  margin: 1rem 0;
+  width: 50%;
+  /* margin: 1rem 0; */
+  /* display: flex; */
+  flex-direction: column;
+  /* justify-content: center; */
 `;
 
 const Ingredient = styled.div`
-  margin: 3rem 0;
+  /* width: 50%; */
+  /* margin: 3rem 0; */
   padding: 1rem;
   border: 0.25rem solid ${COLORS.blue2};
   border-radius: 1rem;
   text-align: center;
   background-color: #ffffff;
+
+  &:first-child {
+    margin-bottom: 1rem;
+  }
+
+  /* &:last-child {
+    margin-left: 1rem;
+  } */
 
   h1 {
     margin-bottom: 1.5rem;
@@ -158,8 +184,6 @@ const Ingredient = styled.div`
 `;
 
 const CookingStepWrapper = styled.div`
-  width: inherit;
-  /* margin: 3rem 0; */
   background-color: #ffffff;
   border: 0.25rem solid ${COLORS.blue2};
   border-radius: 1rem;
@@ -177,7 +201,8 @@ const CookingStep = styled.div`
   align-items: center;
   margin-bottom: 2rem;
   padding: 1rem;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  box-shadow: 0 0.25rem 1rem rgba(0, 0, 0, 0.12),
+    0 0.25rem 0.5rem rgba(0, 0, 0, 0.24);
   border-radius: 1rem;
   background-color: #ffffff;
 
@@ -189,6 +214,18 @@ const CookingStep = styled.div`
     border-radius: 1rem;
     margin-bottom: 1rem;
   }
+`;
+
+const TipWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 2rem;
+  padding: 1rem;
+  border: 0.25rem solid ${COLORS.violet1};
+  border-radius: 1rem;
+  background-color: #ffffff;
+  margin-bottom: 0; // 수정된 부분
 `;
 
 const CommynityWrapper = styled.div``;
