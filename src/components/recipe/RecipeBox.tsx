bba5101165422000bdc011fb14eb68854c1ebfff
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-07-26 00:37:26 +0900

    docs: recipecard 컴포넌트 폴더 이동

--- src/components/recipe/RecipeBox.tsx
+++ src/components/recipe/RecipeBox.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import styled from 'styled-components';
-import RecipeCard from './RecipeCard';
+import RecipeCard from '../common/RecipeCard';
 import COLORS from '../../styles/colors';
 import { Recipe } from '../../types/Recipe';
 
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import COLORS from '../styles/colors';
 import { useRecipeData } from '../hooks/useRecipeData';
 import { Recipe } from '../types/Recipe';
-import RecipeCard from '../components/recipe/RecipeCard';
+import RecipeCard from '../components/common/RecipeCard';
 
 const Search = () => {
   const navigate = useNavigate();
