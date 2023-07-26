28bc48fc2b045dba6d35b4ee698615496e2aed98
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-07-26 23:35:36 +0900

    feat: 세션->로컬 스토리지 저장으로 가입 방식 변경 및 가입 문구 변경

--- src/components/login/SignUpModal.tsx
+++ src/components/login/SignUpModal.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import {
   browserSessionPersistence,
+  browserLocalPersistence,
   createUserWithEmailAndPassword,
   setPersistence,
   updateProfile,
@@ -112,9 +113,38 @@ export const SignUpModal = ({
     }
   };
 
-  // 회원가입 버튼
+  // // 회원가입 버튼
+  // const submitSignUp = () => {
+  //   setPersistence(authService, browserSessionPersistence)
+  //     .then(() => createUserWithEmailAndPassword(authService, email, password))
+  //     .then(() => {
+  //       if (authService.currentUser) {
+  //         updateProfile(authService?.currentUser, {
+  //           displayName: nickname,
+  //         });
+  //       }
+  //     })
+  //     .then(() => {
+  //       alert('회원가입이 완료 되었습니다! 로그인 해주세요.');
+  //       setEmail('');
+  //       setPassword('');
+  //       setConfirmPassword('');
+  //       setNickname('');
+  //       setSignUpModalIsOpen(false);
+  //     })
+  //     .catch((err) => {
+  //       console.log('err: ', err);
+
+  //       if (err.message.includes('already-in-use')) {
+  //         alert('이미 가입된 계정입니다.');
+  //         setEmail('');
+  //       }
+  //     });
+  // };
+
+  // 회원가입 버튼 - 로컬스토리지 저장
   const submitSignUp = () => {
-    setPersistence(authService, browserSessionPersistence)
+    setPersistence(authService, browserLocalPersistence)
       .then(() => createUserWithEmailAndPassword(authService, email, password))
       .then(() => {
         if (authService.currentUser) {
@@ -124,12 +154,13 @@ export const SignUpModal = ({
         }
       })
       .then(() => {
-        alert('회원가입이 완료 되었습니다.');
+        alert('회원가입이 완료 되었습니다! 로그인 해주세요.');
         setEmail('');
         setPassword('');
         setConfirmPassword('');
         setNickname('');
         setSignUpModalIsOpen(false);
+        setLoginModalIsOpen(true);
       })
       .catch((err) => {
         if (err.message.includes('already-in-use')) {
