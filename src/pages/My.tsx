1000e2a7758311b131fd763fae5c2b4faf53cc99
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-07-27 14:58:09 +0900

    design: 프로필 수정 버튼 추가 및 ui 수정

--- src/pages/My.tsx
+++ src/pages/My.tsx
@@ -1,13 +1,23 @@
 import styled from 'styled-components';
 import COLORS from '../styles/colors';
-import { getAuth, signOut } from 'firebase/auth';
-import { useEffect } from 'react';
+import { getAuth, signOut, updateProfile } from 'firebase/auth';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { storage } from '../apis/firebase';
 
 const My = () => {
   // 현재 유저
   const auth = getAuth();
   const user = auth.currentUser;
+
+  // 비로그인시 마이페이지 접근 불가 -> 메인으로
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!user) {
+      navigate('/');
+    }
+  }, []);
 
   // 로그아웃
   const handleLogout = async () => {
@@ -22,13 +32,35 @@ const My = () => {
     }
   };
 
-  // 비로그인시 마이페이지 접근 불가 -> 메인으로
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (!user) {
-      navigate('/');
+  // 프로필 이미지 수정
+  const [photoURL, setPhotoURL] = useState<any>(user?.photoURL);
+  const uploadFB = async (e: any) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setPhotoURL(reader.result);
+    };
+
+    const uploaded_file = await uploadBytes(
+      ref(storage, `images/${e.target.files[0].name}`),
+      e.target.files[0]
+    );
+
+    const file_url = await getDownloadURL(uploaded_file.ref);
+
+    if (user) {
+      updateProfile(user, {
+        photoURL: file_url,
+      })
+        .then(() => {})
+        .catch((error) => {
+          alert('이미지 업로드 실패');
+        });
+    } else {
+      alert('사용자가 로그인되어 있지 않습니다.');
     }
-  }, []);
+  };
 
   return (
     <>
@@ -37,16 +69,17 @@ const My = () => {
           <ProfileBox>
             <Profile>
               <ProfileImg>
-                {<Img src={require('../assets/default_image.png')} />}
+                <Img src={require('../assets/default_image.png')} />
               </ProfileImg>
               <ProfileText>
-                <p>{user?.displayName}</p>
+                <p>{user?.displayName}님 안녕하세요?</p>
               </ProfileText>
-              <ProfileText>
+              {/* <ProfileText>
                 <p>{user?.email}</p>
-              </ProfileText>
+              </ProfileText> */}
             </Profile>
             <LogoutBox>
+              <EditButton onClick={handleLogout}>프로필 수정</EditButton>
               <LogoutButton onClick={handleLogout}>로그아웃</LogoutButton>
             </LogoutBox>
           </ProfileBox>
@@ -89,13 +122,14 @@ const ProfileBox = styled.div`
   width: 25%;
   height: 95%;
   background-color: #fff;
-  border: 0.25rem solid ${COLORS.blue1};
   border-radius: 1rem;
+  box-shadow: 0 0.25rem 1rem rgba(0, 0, 0, 0.12),
+    0 0.25rem 0.5rem rgba(0, 0, 0, 0.24);
 `;
 
 const Profile = styled.div`
   width: 100%;
-  height: 80%;
+  height: 65%;
   display: flex;
   justify-content: space-around;
   align-items: center;
@@ -103,10 +137,8 @@ const Profile = styled.div`
 `;
 
 const ProfileImg = styled.div`
-  width: 60%;
-  height: 55%;
+  width: 55%;
   border-radius: 50%;
-  margin: 1rem 0;
 `;
 
 const Img = styled.img`
@@ -123,30 +155,47 @@ const ProfileText = styled.div`
 
 const LogoutBox = styled.div`
   width: 100%;
-  height: 20%;
+  height: 35%;
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
+
   align-items: center;
+  flex-direction: column;
 `;
 
-const LogoutButton = styled.button`
+const EditButton = styled.button`
   width: 80%;
   height: 4.65rem;
   border-radius: 1rem;
   border: 0.25rem solid ${COLORS.blue1};
   font-size: 1.5rem;
   background-color: ${COLORS.blue1};
-  color: white;
+  color: #fff;
+  cursor: pointer;
+`;
+
+const LogoutButton = styled.button`
+  outline: none;
+  width: 80%;
+  height: 4.65rem;
+  border-radius: 1rem;
+  border: 0.15rem solid ${COLORS.blue1};
+  font-size: 1.5rem;
+  background-color: ${COLORS.backGround};
+  color: ${COLORS.blue1};
   cursor: pointer;
   outline: none;
 `;
 
 const UserHistoryBox = styled.div`
-  width: 50%;
+  width: 70%;
   height: 95%;
+
   background-color: #fff;
-  border: 0.25rem solid ${COLORS.blue1};
   border-radius: 1rem;
+
+  box-shadow: 0 0.25rem 1rem rgba(0, 0, 0, 0.12),
+    0 0.25rem 0.5rem rgba(0, 0, 0, 0.24);
 `;
 
 const UserHistory = styled.div`
