81f1b770838128b2b80efc34c1956028bb6f0bb9
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-07-28 21:51:12 +0900

    docs: 문서명 수정 등

--- src/components/recipe/RecipeBox.tsx
+++ src/components/recipe/RecipeBox.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 import RecipeCard from '../common/RecipeCard';
 import COLORS from '../../styles/colors';
