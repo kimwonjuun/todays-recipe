26da72645488264089f250f302a1ac4d578ff521
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-07-28 22:17:23 +0900

    feat: 로딩 컴포넌트 완성

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,3 +1,36 @@
+import styled, { keyframes } from 'styled-components';
+import COLORS from '../../styles/colors';
+
 export const Loading = () => {
-  return <></>;
+  return (
+    <>
+      <Wrapper>
+        <LoadingImage
+          src={require('../../assets/loading.png')}
+          alt="Loading..."
+        />
+      </Wrapper>
+    </>
+  );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - 12.8rem);
+  background-color: ${COLORS.backGround};
+`;
+
+const bounce = keyframes`
+  0%, 100% { transform: translateY(0); }
+  50% { transform: translateY(-10px); }
+`;
+
+const LoadingImage = styled.img`
+  width: 15rem;
+  height: 15rem;
+  animation: ${bounce} 0.8s infinite ease-in-out;
+`;
