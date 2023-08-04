2fec39e6455ab00178cc59b1243580e443bce07e
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-08-04 20:03:07 +0900

    refactor: 로그인, 회원가입 모달 - 리팩토링

--- src/components/Auth/SignUpModal.tsx
+++ src/components/Auth/SignUpModal.tsx
@@ -6,19 +6,9 @@ import {
   updateProfile,
 } from 'firebase/auth';
 import { authService } from '../../apis/firebase';
-import {
-  BottomWrapper,
-  Button,
-  CloseButton,
-  CustomSpan,
-  Input,
-  InputWrapper,
-  LoginText,
-  Modal,
-  ModalWrapper,
-  TitleWrapper,
-} from '../../styles/authModalStyles';
 import { emailRegex, passwordRegex } from '../../utils/regex';
+import COLORS from '../../styles/colors';
+import { styled } from 'styled-components';
 
 export const SignUpModal = ({
   setLoginModalIsOpen,
@@ -222,3 +212,126 @@ export const SignUpModal = ({
     </>
   );
 };
+
+const ModalWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+`;
+
+const Modal = styled.div`
+  background-color: white;
+  padding: 2rem;
+  border-radius: 1rem;
+  border: 0.25rem solid ${COLORS.blue1};
+  width: 40rem;
+  height: 50rem;
+  position: relative;
+  text-align: center;
+`;
+
+const TitleWrapper = styled.div`
+  width: inherit;
+  height: 5rem;
+  font-size: 2.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const InputWrapper = styled.div`
+  width: inherit;
+  height: 35rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  width: 30rem;
+  height: 4rem;
+  border-radius: 1rem;
+  border: 0.25rem solid ${COLORS.blue1};
+  font-size: 1.5rem;
+  outline: none;
+  text-align: left;
+  padding-left: 1rem;
+`;
+
+const BottomWrapper = styled.div`
+  width: inherit;
+  height: 10rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  width: 31.5rem;
+  height: 4.5rem;
+  border-radius: 1rem;
+  border: ${({ disabled }) =>
+    disabled ? 'none' : `0.25rem solid ${COLORS.blue1}`};
+  font-size: 2rem;
+  color: #fff;
+  background-color: ${({ disabled }) =>
+    disabled ? COLORS.gray : COLORS.blue1};
+  cursor: ${({ disabled }) => (disabled ? 'default' : 'pointer')};
+  outline: none;
+  &:hover {
+    background-color: ${({ disabled }) =>
+      disabled ? COLORS.gray : COLORS.blue2};
+  }
+`;
+
+const LoginText = styled.div`
+  font-size: 1.5rem;
+  cursor: pointer;
+  &:hover {
+    color: ${COLORS.blue2};
+  }
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+  font-size: 2rem;
+  background: none;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  z-index: 10;
+  &:hover {
+    color: ${COLORS.blue2};
+  }
+`;
+
+const CustomSpan = styled.span`
+  font-size: 1rem;
+  margin: -2.2rem 0;
+  &.success {
+    color: ${COLORS.blue1};
+  }
+  &.error {
+    color: ${COLORS.red};
+  }
+`;
+
+const Logo = styled.div`
+  width: 11rem;
+  height: 11rem;
+`;
+
+const LogoImg = styled.img`
+  width: 100%;
+`;
