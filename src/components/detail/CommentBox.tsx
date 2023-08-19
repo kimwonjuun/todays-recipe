9c81b5e60ab3c9f5b1b5f7950f2ca49906a1fdbe
kimwonjuun <117059038+kimwonjuun@users.noreply.github.com>
2023-08-20 04:08:33 +0900

    fix: resolved #51 - 댓글 중복 생성 방지 위한 state 생성, 코드 수정

--- src/components/detail/CommentBox.tsx
+++ src/components/detail/CommentBox.tsx
@@ -32,6 +32,9 @@ const CommentBox = ({
   // 댓글
   const [commentsList, setCommentsList] = useState<UserCommentProps[]>([]);
 
+  // 댓글 중복 생성 방지를 위한 state 생성
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+
   // 댓글 인풋: useInput
   const { inputValue, setInputValue, handleInputChange } = useInput('');
 
@@ -39,13 +42,24 @@ const CommentBox = ({
   const handleCommentSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
+    // 제출 중이면 반환하여 중복 제출 방지
+    if (isSubmitting) {
+      return;
+    }
+
+    // 제출 시작 시 상태를 true로 설정해 제출이 일어나지 않도록 함
+    setIsSubmitting(true);
+
     if (!currentUserUid) {
       openAlert('댓글은 로그인 후 작성이 가능합니다.');
+      setIsSubmitting(false);
       return;
     }
 
     if (!inputValue || !inputValue.trim()) {
       openAlert('댓글을 입력해주세요.');
+      // alert 후 상태를 다시 false로 설정해 제출이 가능하게 함
+      setIsSubmitting(false);
       return;
     }
 
@@ -57,7 +71,7 @@ const CommentBox = ({
       .then((userDoc) => {
         const userData = userDoc.data();
 
-        // 댓글 객체를 생성
+        // 댓글 객체 생성
         const newComment = {
           uid: currentUserUid,
           nickname: user?.displayName,
@@ -68,14 +82,14 @@ const CommentBox = ({
           updatedAt: Date.now(),
         };
 
-        // 'user-comments' 필드에 존재하는 배열에 새 댓글을 추가하고 문서 업데이트
+        // 'user-comments' 필드에 존재하는 배열에 새 댓글 추가하고 문서 업데이트
         if (userData) {
           const userComments = userData['user-comments'] ?? [];
           return updateDoc(userDocRef, {
             'user-comments': [...userComments, newComment],
           });
         } else {
-          // 문서가 존재하지 않으면 새 문서를 생성 후 댓글 추가
+          // 문서가 존재하지 않으면 새 문서 생성 후 댓글 추가
           const newUserDoc = {
             'user-comments': [newComment],
           };
@@ -88,6 +102,10 @@ const CommentBox = ({
       })
       .catch((error) => {
         openAlert('댓글 저장 실패.');
+      })
+      .finally(() => {
+        // 제출이 완료되면 플래그 상태를 false로 설정
+        setIsSubmitting(false);
       });
   };
 
